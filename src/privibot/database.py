40825eeeca0e612251c072a31e470862d51f37b8
Timothée Mazzucotelli <pawamoy@pm.me>
2019-10-27 15:57:38 +0100

    style: Lint

--- src/privibot/database.py
+++ src/privibot/database.py
@@ -2,7 +2,6 @@ from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, UniqueConst
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 
-
 Base = declarative_base()
 session = None
 
